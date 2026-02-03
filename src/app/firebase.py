a209ba4c5bfe4377ed4d4652ff82ce52af2930ae
vincentabacalso-spec <vincentabacalso@su.edu.ph>
2026-02-03 22:47:34 +0800

    Update firebase.py

--- src/app/firebase.py
+++ src/app/firebase.py
@@ -2,10 +2,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os 
 from dotenv import load_dotenv
+import json 
 
 load_dotenv()
 
-FIREBASE_ADMIN_SDK_PATH = os.getenv("FIREBASE_ADMIN_SDK_PATH")
+FIREBASE_ADMIN_SDK_PATH = json.loads(os.getenv("FIREBASE_ADMIN_SDK_PATH"))
 
 cred = credentials.Certificate(FIREBASE_ADMIN_SDK_PATH)
 firebase_admin.initialize_app(cred)
